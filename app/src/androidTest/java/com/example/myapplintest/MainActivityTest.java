539a0794fed1107243b66643f22453af25fa31bc
Omar Win10 <omar_desarrollo@yahoo.com>
2021-02-11 13:32:15 -0600

    [UI] Espresso test to validate txt in screen.
     On branch master
     Changes to be committed:
    	modified:   app/build.gradle
    	new file:   app/src/androidTest/java/com/example/myapplintest/MainActivityTest.java

---
+++ app/src/androidTest/java/com/example/myapplintest/MainActivityTest.java
@@ -0,0 +1,27 @@
+package com.example.myapplintest;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.myapplintest.view.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule rule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void verifyNumberItemsIsShowing(){
+        Espresso.onView(withId(R.id.ll_txt_list_products)).check(matches(ViewMatchers.isDisplayed()));
+    }
+}
